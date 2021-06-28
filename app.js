9be1a386cf55e1b7a69dac92dcc00583a46a896f
unknown <xlanimation@gmail.com>
2021-06-28 13:11:10 -0500

    fixed async/await section

--- app.js
+++ app.js
@@ -39,17 +39,19 @@
 //     console.log(err);
 // });
 
-async function doMath(result, result) {
+async function doMath(x, y) {
     try {
-        let response = await slowMath.add(result, result);
-        // let response4 = await slowMath.subtract(response3, 3);
-        // let response5 = await slowMath.add(response4, 98);
-        // let response6 = await slowMath.remainder(response5, 2);
-        // let response7 = await slowMath.multiply(response6, 50);
-        // let response8 = await slowMath.remainder(response7, 40);
-        // let response9 = await slowMath.add(response8, 32);
+        let response = await slowMath.add(x, y);
+        let response2 = await slowMath.multiply(response, 2);
+        let response3 = await slowMath.divide(response2, 4);
+        let response4 = await slowMath.subtract(response3, 3);
+        let response5 = await slowMath.add(response4, 98);
+        let response6 = await slowMath.remainder(response5, 2);
+        let response7 = await slowMath.multiply(response6, 50);
+        let response8 = await slowMath.remainder(response7, 40);
+        let response9 = await slowMath.add(response8, 32);
 
-        console.log(`The final result is ${response}.`);
+        console.log(`The final result is ${response9}.`);
 
     } catch (err) {
         console.log(err);
@@ -57,4 +59,4 @@ async function doMath(result, result) {
 
 }
 
-doMath(3, 3);
+doMath(6, 2);
